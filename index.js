45587813e90327808c71feecf06dea950667c6dc
Alexey Shmakov <razzeeyy@gmail.com>
2019-09-28 03:19:12 +0300

    use es6+ features where appropriate

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-var os = require('os')
-var path = require('path')
-var express=require("express")
-var serveStatic = require('serve-static')
-var multer  = require('multer')
+const os = require('os')
+const path = require('path')
+const express = require("express")
+const serveStatic = require('serve-static')
+const multer  = require('multer')
 
-DEFAULT_OPTS = {
+const DEFAULT_OPTS = {
   destination: path.join(os.tmpdir(), 'fshare'),
   limits: null
 }
 
-module.exports = function(opts){
+module.exports = function(opts) {
   opts = Object.assign({}, DEFAULT_OPTS, opts) 
 
-  var storage = multer.diskStorage({
+  const storage = multer.diskStorage({
     destination: opts.destination,
-    filename: function (req, file, cb) {
-      //TODO: will it clash on muptiple requests at once?
-      cb(null, Date.now().toString(16)+path.extname(file.originalname))
-    }
+    //TODO: will it clash on muptiple requests at once?
+    filename: (req, file, cb) => cb(null, Date.now().toString(16)+path.extname(file.originalname))
   })
-  var upload = multer({ storage: storage, limits: opts.limits })
+  const upload = multer({ storage: storage, limits: opts.limits })
 
-  var fshare = express.Router()
+  const fshare = express.Router()
 
   fshare.use('/', serveStatic(opts.destination))
 
-  fshare.post('/', upload.single('file'), function(req,res){
+  fshare.post('/', upload.single('file'), (req,res) => {
       res.set('Connection', 'close');
       res.end(req.file.filename)
   })
